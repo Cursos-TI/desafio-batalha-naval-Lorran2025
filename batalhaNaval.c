00b716709b0090e2d61c50fa2e2aebdbcf37ba34
Lorran2025 <lorranpereiraslv@gmail.com>
2025-04-30 16:12:07 +0000

    Adicionando, cone, cruz e octaedro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
 
 int main(){
-
+    //Letras das colunas do tabuleiro
     char matriz[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int tabuleiro[10][10] = {0};
-    int i,j;
+    int i, j;
+    //Posição das habilidades
+    int navioConeX = 4;
+    int navioConeY = 4;
+    int navioCruz = 4;
+    int navioOctaedroX = 6;
+    int navioOctaedroY = 1;
 
-    int navioVertical[2] = {3,3};
-    int navioDiagonal[2] = {3,3};
-
-    //Posições dos navios
-    int linhaNavioV = 5;
-    int colunaNavioV = 7;
-    int linhaNavioD = 1;
-    int colunaNavioD = 3;
-
-    for (int i = 0; i < 2; i++){
-        tabuleiro[linhaNavioV + i][colunaNavioV] = navioVertical[i];
-    }
-
-    for (int i = 0; i < 2; i++){
-        tabuleiro[linhaNavioD + i][colunaNavioD + i] = navioDiagonal[i];
+    //Posição do Cone
+    for (j = 0; j < 5; j++){
+        tabuleiro[navioConeX + 2][navioConeY + j] = 3;
+        tabuleiro[navioConeX][navioConeY + 2] = 3;
+        tabuleiro[navioConeX + 1][navioConeY + 1] = 3;
+        tabuleiro[navioConeX + 1][navioConeY + 2] = 3;
+        tabuleiro[navioConeX + 1][navioConeY + 3] = 3;
+        tabuleiro[navioConeX + 2][navioConeY] = 3;
     }
     
+    //Posição do Octaedro
+    if (navioOctaedroX >= 0 && navioOctaedroX < 10 && navioOctaedroY >= 0 && navioOctaedroY < 10) {
+        tabuleiro[navioOctaedroX][navioOctaedroY] = 3;
+        tabuleiro[navioOctaedroX + 1][navioOctaedroY - 1] = 3;
+        tabuleiro[navioOctaedroX + 1][navioOctaedroY] = 3;
+        tabuleiro[navioOctaedroX + 1][navioOctaedroY + 1] = 3;
+        tabuleiro[navioOctaedroX + 2][navioOctaedroY] = 3;
+        }
+    
+    //Posição da Cruz
+    for (j = 0; j < 5; j++){
+        tabuleiro[navioCruz - 3][j] = 3;
+    }
+
+    for (i = 0; i < 3; i++){
+        tabuleiro[i][navioCruz - 2] = 3;
+    }
+    //Impressão do tabuleiro
     printf(" *** TABULEIRO BATALHA NAVAL ***\n");
     printf("  ");
     for (i = 0; i < 10; i++){
